2451bf8e42201d45cb4f1b431537e5074a107029
Timka-Sawyer <timka-sawyer@yandex.ru>
2021-02-11 21:27:08 +0300

    cannot GET /list

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+
 
 const App = (props) => {
   return (
-    <MainScreen films={props.films} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/">
+          <MainScreen films={props.films} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
